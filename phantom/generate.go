e320d65189884cd603e030fa67518e0bae6a240d
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-10-30 00:00:09 +0900

    fix

--- phantom/generate.go
+++ phantom/generate.go
@@ -10,19 +10,19 @@ func generatePhantomPath() string {
 	dir, _ := os.Getwd()
 
 	if runtime.GOOS == "darwin" {
-		return filepath.Join(dir, "resource", "bin", "darwin", "phantomjs")
+		return filepath.Join(dir, "..", "resource", "bin", "darwin", "phantomjs")
 	} else if runtime.GOOS == "windows" {
-		return filepath.Join(dir, "resource", "bin", "windows", "phantomjs.exe")
+		return filepath.Join(dir, "..", "resource", "bin", "windows", "phantomjs.exe")
 	} else {
 		if runtime.GOARCH == "386" {
-			return filepath.Join(dir, "resource", "bin", "linux386", "phantomjs")
+			return filepath.Join(dir, "..", "resource", "bin", "linux386", "phantomjs")
 		} else {
-			return filepath.Join(dir, "resource", "bin", "linux64", "phantomjs")
+			return filepath.Join(dir, "..", "resource", "bin", "linux64", "phantomjs")
 		}
 	}
 }
 
 func generateJSPath() string {
 	dir, _ := os.Getwd()
-	return filepath.Join(dir, "resource", "js", "load.js")
+	return filepath.Join(dir, "..", "resource", "js", "load.js")
 }
